a479adfc3305283268073e324421cc956d0d96c8
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2026-04-14 16:11:18 +0200

    [*]Verbose mode

--- lanch-seg.py
+++ lanch-seg.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
 from os.path import join, isdir, basename
 from os import mkdir
 from tifffile import imread, imwrite
@@ -8,8 +8,18 @@ import yaml
 from methods import MethodSelecter
 from time_decorator import Timer
 
-
 METHODS_AVAILABLE = ["SD3D", "CP2.5D", "CP-SAM", "uSeg2.5D", "OP3D", "StitchCP2D", "StitchSD2D"]
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ArgumentTypeError('Boolean value expected.')
+
 
 if __name__ == "__main__":
 
@@ -17,12 +27,15 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input")
     parser.add_argument("-m", "--method")
     parser.add_argument("-o", "--output")
+    parser.add_argument("-v", "--verbose", type=str2bool, default=True)
 
     args = parser.parse_args()
     
     input_dir = args.input
     output_dir = args.output
     method_name = args.method
+
+    verbose = args.versbose
 
     assert method_name in METHODS_AVAILABLE, f"{method_name} is an unknown method\nknown methods are {METHODS_AVAILABLE}"
 
@@ -33,12 +46,17 @@ if __name__ == "__main__":
     with open(join("methods_config",f"{method_name.replace(".","")}.yaml"), "r") as f_cfg:
         method_cfg = yaml.safe_load(f_cfg)
 
+    if verbose: print(f"Setting up the --{method_name}-- model.")
     method = MethodSelecter(method_name, config = method_cfg)
 
     images_path = glob(join(input_dir, "*.tif"))
 
 
     for img_f in images_path:
+
+        im_name = basename(img_f)
+        if verbose: print(f"Processing image {im_name}")
+
         img = imread(img_f)
 
         with Timer():
