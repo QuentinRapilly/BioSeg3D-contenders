3d3532f8c0648e464a3ae5233efdbe89a00b43e6
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2026-04-20 11:18:54 +0200

    [*]modif on SD3D train

--- training/train_SD3D.py
+++ training/train_SD3D.py
@@ -115,9 +115,10 @@ def train(cfg : DictConfig):
     if use_gpu:
         from csbdeep.utils.tf import limit_gpu_memory
         # adjust as necessary: limit GPU memory to be used by TensorFlow to leave some to OpenCL-based computations
-        limit_gpu_memory(0.8)
+        limit_gpu_memory(fraction = 0.8, total_memory = 75000)
 
-    model = StarDist3D(conf, name='stardist', basedir='models')
+
+    model = StarDist3D(conf, name=cfg["storage_dir"], basedir=cfg["storage_dir"])
 
 
     model.train(X_trn, Y_trn, validation_data=(X_val,Y_val), augmenter=augmenter)
